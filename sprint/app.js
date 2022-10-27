9ef7f348ae09a863072033218b8b3386827c4ed5
MakenzieRoberts <makenzie.roberts@keyin.com>
2022-10-27 17:49:26 -0230

    Removed validation for now, updated comments

--- sprint/app.js
+++ sprint/app.js
@@ -1,14 +1,11 @@
 const express = require("express");
-
 const path = require("path");
-
-// const db = require("./db/queries");
-
 const app = express();
 
 // import stack module
 const { handleStack } = require("./stack.js");
 const { handleQueue } = require("./queue.js");
+const pool = require("./config.js").pool;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "./public")));
@@ -17,121 +14,99 @@ app.get("/", function (req, res) {
 	res.sendFile(path.join(__dirname, "./public/form.html"));
 });
 
-const pool = require("./config.js").pool;
-
 const getMessage = (request, response) => {
-	// This will pull the id AND PARSE IT TO A NUMBER
-	// const id = parseInt(request.params.id); // This was for path mapping (users/1). we're changing it to param (users?id=1)
+	// When a user puts in a request for a message, they ust input their agent ID number
+	// and the ID of the data structure they want to retrieve from
+
+	// This is grabbing the agent_id and structure_id columns from the request
 	const retrieving_agent_id = request.query.id;
-	const structure = parseInt(request.query.structure); // This is the parameter method
+	const structure = parseInt(request.query.structure);
+
+	// Logging the input values to the console
 	console.log("Retrieving agent ID:", retrieving_agent_id);
 	console.log("Structure:", structure);
-	if (structure != 1 || structure != 2) {
-		response
-			.status(200)
-			.send(
-				`Invalid Data Structure. <br> Please Enter 1 to receive a message from the stack, or 2 to receive a message from the queue.`
-			);
-	} else {
-		pool.query(
-			"SELECT * FROM secret.messages WHERE structure_id = $1",
-			[structure],
-			(error, results) => {
-				if (error) {
-					throw error;
-				}
-				let records = results.rows;
-				// This console log is how we can access each individual property in the json object
-				// So if we want to display all messages nicely in the browser, we can use the .map() function to iterate through the array and display each property
-				console.log("Results rows: \n", records);
 
-				// !TODO: Maybe this is where the stack/queue functions should go? Like, we'll
-				// get rid of the "by id" parts so it returns all, and then we can use our
-				// stack/queue functions to sort the data to be displayed the data in the
-				// browser
-				//!TODO: add if statement to check structure type and then call the appropriate function
+	// This if-statement makes sure the user is entering a valid data structure ID. If
+	// they enter anything other than 1 or 2, the response sent back is a string containing an error message.
 
-				if (structure === 1) {
-					console.log("Structure 1 (Stack) Selected");
-					let stackResult = handleStack(records);
-
-					if (stackResult === undefined) {
-						console.log("No data found for structure 1");
-						response.status(200).send("No data found for structure 1");
-					} else {
-						let parsedResult = JSON.parse(stackResult);
-						console.log("Stack result:", parsedResult);
-
-						const message_id = parsedResult.message_id;
-						const retrieved_data = parsedResult.data;
-
-						// ADD TO RETRIEVALS TABLE
-						addRetrieval(retrieving_agent_id, retrieved_data, structure);
-
-						// SEND RESPONSE
-						response.status(200).send(`DATA: ${parsedResult.data}`);
-						console.log(
-							"Message data displayed in browser: '" + parsedResult.data + "'"
-						);
-
-						// DELETE FROM MESSAGES TABLE
-						deleteMessage(message_id);
-
-						response.end();
-					}
-				} else if (structure === 2) {
-					console.log("Structure 2 (Queue) Selected");
-					let queueResult = handleQueue(records);
-
-					if (queueResult === undefined) {
-						console.log("No data found for structure 2");
-						response.status(200).send("No data found for structure 2");
-					} else {
-						let parsedResult = JSON.parse(queueResult);
-						console.log("Queue result:", parsedResult);
-
-						const message_id = parsedResult.message_id;
-						const retrieved_data = parsedResult.data;
-
-						// ADD TO RETRIEVALS TABLE
-						addRetrieval(retrieving_agent_id, retrieved_data, structure);
-
-						// SEND RESPONSE
-						response.status(200).send(`DATA: ${parsedResult.data}`);
-						console.log(
-							"Message data displayed in browser: ",
-							parsedResult.data
-						);
-
-						// DELETE FROM MESSAGES TABLE
-						deleteMessage(message_id);
-
-						response.end();
-					}
-				}
-				//!TODO: Add agent id, structure id, and data retrieved to new table called "retrieved"
-				//!TODO: Then delete the retrived message from messsages table
-				// response.status(200).send(`Results: ${JSON.stringify(results.rows)}`);
-
-				// response.status(200).send(
-				// 	`<b>Results:</b> <br>
-				//  	 	ID: 		${JSON.stringify(records[0].id)} <br>
-				// 	 	Name: 		${JSON.stringify(records[0].name)} <br>
-				// 	 	Course Id: 	${JSON.stringify(records[0].course_id)}`
-				// );
-
-				// !TODO: Remember to hide the agent_id from the receiver! (?)
-				// response
-				// 	.status(200)
-				// 	.send(
-				// 		records.map(
-				// 			(item) =>
-				// 				`AGENT_ID: ${item.agent_id}, DATA: ${item.data}, STRUCTURE: ${item.structure_id}`
-				// 		)
-				// 	);
+	// Here we are using a select statement to grab only the records from the messages
+	// table that have the same structure_id as the one the user entered.
+	pool.query(
+		"SELECT * FROM secret.messages WHERE structure_id = $1",
+		[structure],
+		(error, results) => {
+			if (error) {
+				throw error;
 			}
-		);
-	}
+
+			// Now we have a variable containing the results of the query. Below, this
+			// variable is passed into either the handleStack or handleQueue function,
+			// depending on the structure ID the user entered. depending on the
+			// structure ID the user entered.
+			let records = results.rows;
+
+			// This console log is how we can access each individual property in the json object
+			// So if we want to display all messages nicely in the browser, we can use the .map() function to iterate through the array and display each property
+			console.log("Results rows: \n", records);
+
+			// This if-statement checks structure type and then call the appropriate function.
+			// If the structure is 1, the stack function is called. If the structure is 2, the queue function is called.
+			if (structure === 1) {
+				console.log("Structure 1 (Stack) Selected");
+				let stackResult = handleStack(records);
+				if (stackResult === null) {
+					console.log("No data found in structure 1");
+					response.status(200).send("No data found for structure 1");
+				} else {
+					let parsedResult = JSON.parse(stackResult);
+					console.log("Stack result:", parsedResult);
+
+					const message_id = parsedResult.message_id;
+					const retrieved_data = parsedResult.data;
+
+					// ADD TO RETRIEVALS TABLE
+					addRetrieval(retrieving_agent_id, retrieved_data, structure);
+
+					// SEND RESPONSE
+					response.status(200).send(`DATA: ${parsedResult.data}`);
+					console.log(
+						"Message data displayed in browser: '" + parsedResult.data + "'"
+					);
+
+					// DELETE FROM MESSAGES TABLE
+					deleteMessage(message_id);
+
+					response.end();
+				}
+			} else if (structure === 2) {
+				console.log("Structure 2 (Queue) Selected");
+				let queueResult = handleQueue(records);
+
+				if (queueResult === null) {
+					console.log("No data found for structure 2");
+					response.status(200).send("No data found in structure 2");
+				} else {
+					let parsedResult = JSON.parse(queueResult);
+					console.log("Queue result:", parsedResult);
+
+					const message_id = parsedResult.message_id;
+					const retrieved_data = parsedResult.data;
+
+					// ADD TO RETRIEVALS TABLE
+					addRetrieval(retrieving_agent_id, retrieved_data, structure);
+
+					// SEND RESPONSE
+					response.status(200).send(`DATA: ${parsedResult.data}`);
+					console.log("Message data displayed in browser: ", parsedResult.data);
+
+					// DELETE FROM MESSAGES TABLE
+					deleteMessage(message_id);
+
+					response.end();
+				}
+			}
+		}
+	);
 };
 
 // OLD before making one with request and response
@@ -165,33 +140,26 @@ function deleteMessage(message_id) {
 
 // // Add
 const addMessage = (request, response) => {
-	// this is grabbing the name and email columns from the request body
-	console.log(request.body);
 	const { id, data, structure } = request.body;
+
 	// And then we take those and use an insert statement to add some data those columns/add a new record
 	// The $ are binding our id, name and course_id variables. $1 = user the first variable, $2 = use the second, etc. So $1 = id value, $2 = name value, etc.
-	if (structure != 1 || structure != 2) {
-		response
-			.status(200)
-			.send(
-				`Invalid Data Structure. <br> Please Enter 1 to add your message to the stack, or 2 to add your message to the queue.`
-			);
-	} else {
-		pool.query(
-			"INSERT INTO secret.messages (agent_id, data, structure_id) VALUES ($1, $2, $3)",
-			[id, data, structure],
-			(error, results) => {
-				if (error) {
-					throw error;
-				}
-				response
-					.status(201)
-					.send(
-						`User added with ID: ${id}, DATA: ${data}, STRUCTURE: ${structure}`
-					);
+	console.log("ADDMESSAGE STRUCTURE", structure);
+
+	pool.query(
+		"INSERT INTO secret.messages (agent_id, data, structure_id) VALUES ($1, $2, $3)",
+		[id, data, structure],
+		(error, results) => {
+			if (error) {
+				throw error;
 			}
-		);
-	}
+			response
+				.status(201)
+				.send(
+					`User added with ID: ${id}, DATA: ${data}, STRUCTURE: ${structure}`
+				);
+		}
+	);
 };
 
 app.get("/view/", getMessage);
